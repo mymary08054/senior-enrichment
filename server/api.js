9909883c6fa3b2597ff7e292c5bdbe33da877926
Mary Yen <mymaryyn@gmail.com>
2017-08-27 23:11:43 -0400

    work

--- server/api.js
+++ server/api.js
@@ -13,15 +13,58 @@ api.get('/campuses', (req, res, next) => {
 })
 
 api.get('/campuses/:campusId', (req, res, next) => {
-	Campus.findOne({id: req.params.campusId})
+	Campus.findOne({ id: req.params.campusId })
 		.then(campuses => res.json(campuses))
 		.catch(next);
 })
 
 api.post('/campuses', function (req, res, next) {
 	Campus.create(req.body)
-  .then(campus => res.status(201).json(campus))
-  .catch(next);
+		.then(campus => res.status(201).json(campus))
+		.catch(next);
+});
+
+api.put('/campuses/:campusId', function (req, res, next) {
+	Campus.update(req.body, { where: { id: req.params.campusId } })
+		.then(campus => res.status(200).json(campus))
+		.catch(next);
+});
+
+api.delete('/campuses/:campusId', function (req, res, next) {
+	Campus.destroy({ where: { id: req.params.campusId } })
+		.then(() => res.status(204).end())
+		.catch(next);
+});
+
+///////Student/////////
+api.get('/students', (req, res, next) => {
+	Student.findAll()
+		.then(students => res.json(students))
+		.catch(next);
+})
+
+api.get('/students/:studentId', (req, res, next) => {
+	Student.findOne({ id: req.params.studentId })
+		.then(students => res.json(students))
+		.catch(next);
+})
+
+api.post('/students', function (req, res, next) {
+	Student.create(req.body)
+		.then(student => res.status(201).json(student))
+		.catch(next);
+});
+
+api.put('/students/:studentId', function (req, res, next) {
+	Student.update(req.body, { where: { id: req.params.studentId } })
+		.then(student => res.status(200).json(student))
+		.catch(next);
+});
+
+api.delete('/students/:studentId', function (req, res, next) {
+	Student.destroy({ where: { id: req.params.studentId } })
+		.then(() => res.status(204).end())
+		.catch(next);
 });
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
